1e5e31f0ffef3fef67cc7792e0d6175c76e8739e
Alex <32684783+Pika98@users.noreply.github.com>
2018-01-11 06:50:27 +0000

    update?!

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
+var currentSlide = 0;
+var interval = 4000;
+
+var slides = document.getElementsByClassName("shopSlide");
 function init()
 {
 	console.log("init");
-	document.getElementById("#colourStatus").innerHTML = "Gold";
+	console.log("end");
+}
+
+function slideShow()
+{
+	console.log("slides_js loaded");
+	slides[currentSlide].className = 'shopSlide';
+	currentSlide = (currentSlide+1)%slides.length;
+	slides[currentSlide].className = 'shopSlide showing';
+	setTimeout(slideShow, interval);
+}
+
+function sliderColour()
+{
+	console.log("sliderColour loaded")
 }
 window.onload = function()
 {
 	console.log("Script Loaded");
 	init();
+	slideShow();
+	sliderColour()
 };
 
